0bce504efc43c6e796263b285e55fc8dbba35517
Hannan Butt <hannanbutt1256@gmail.com>
2024-11-18 21:07:07 +0500

    changes

--- src/components/Protected.tsx
+++ src/components/Protected.tsx
@@ -9,7 +9,7 @@ const Protected: React.FC<ProtectedProps> = ({ children }) => {
   const { user } = useAuth();
 
   if (!user) {
-    return <Navigate to="/auth/login" replace />;
+    return <Navigate to="auth/login" />;
   }
 
   return children;
